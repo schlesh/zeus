49feaa3336fc98ee76b8479f988afd35a0d0b3de
kmorrison <fspoon64+git@gmail.com>
2020-11-19 17:36:13 -0800

    Initial opendota commit

---
+++ opendota.py
@@ -0,0 +1,18 @@
+
+import requests
+
+import secret
+
+#API_ROOT = 'https://api.opendota.com/api/matches/271145478?api_key=YOUR-API-KEY'
+API_ROOT = 'https://api.opendota.com/api'
+
+def make_example_call():
+    response = requests.get(
+        f'{API_ROOT}/matches/271145478',
+        params=dict(api_key=secret.OPENDOTA_API_KEY),
+    )
+    print(response.json())
+
+if __name__ == '__main__':
+    make_example_call()
+
